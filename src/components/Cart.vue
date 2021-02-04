8e93dbe88a3bead771f63ff35245f4baa0fb579e
DESKTOP-6V70R8A\DCOM863 <614234013@parichat.skru.ac.th>
2021-02-04 10:34:35 +0700

    4/2/2564

---
+++ src/components/Cart.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+    <h2>Books in Cart</h2>
+    <div v-bind:key="book.bookid" v-for="book in cart">
+        <CartItem v-bind:book="book" v-on:removeCartitem="Remove(book.bookid)" />
+    </div>
+</div>
+</template>
+
+<script>
+import CartItem from './CartItem.vue';
+export default {
+    name: "Cart",
+    props: ["cart"],
+    components: {
+        CartItem
+    },
+    methods:{
+        Remove(bookId){
+            this.$emit('removeCartitem',bookId);
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.book-item {
+    background: #ff7300;
+    padding: 10px;
+    border-bottom: 1px rgb(0, 0, 0) dotted;
+}
+</style>
---
+++ src/components/CartItem.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="book-item" v-bind:class="{'is-complete':book.completed}">
+
+    <b>{{book.title}}</b><br />
+    <b>Price:</b> {{book.price}}<br />
+    <b>Qty:</b> {{book.qty}}<br />
+    <div align="center">
+        <button class="btn btn-success" >&nbsp;+&nbsp;</button>&nbsp;
+        <button class="btn btn-info">&nbsp;-&nbsp;</button>&nbsp;
+        <button class="btn btn-danger" v-on:click="Remove(book.bookid)">&nbsp;x&nbsp;</button>
+    </div>
+
+</div>
+</template>
+
+<script>
+export default {
+    name: "CartItem",
+    props: ["book"],
+    methods:{
+        Remove(bookId){
+            this.$emit('removeCartitem',bookId)
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.book-item {
+    background: #dcf83e;
+    padding: 10px;
+    border-bottom: 1px rgb(0, 0, 0) dotted;
+}
+</style>
